98efbd1cc2e644c4637839fdcf7cd5db3526b748
xuzhen <15064015617@163.com>
2017-06-30 10:35:57 +0800

    eclipse git修改提交测试!

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class Hello{
   public static void main(String[] args){
-    System.out.println("why?");
-    System.out.println("Hello Java!!!!");
+    System.out.println("你说的好像不对呀!");
   }
 }
